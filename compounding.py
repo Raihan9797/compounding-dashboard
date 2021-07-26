08be481c07713f048e7cb6f77591d1fb1d926a9f
Raihan9797 <raihanmuhd97@gmail.com>
2021-07-26 21:58:32 +0800

    renamed to compounding.py; rmv unused libraries; add code for hosting to heroku

--- compounding.py
+++ compounding.py
@@ -1,15 +1,15 @@
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
 ### create app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+server = app.server
 
 
 ### import and clean data
